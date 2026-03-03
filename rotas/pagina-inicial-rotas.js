cbcc6ccd46bbc620e048f4143e69b712b3bbb66d
CEVPr <ceduardovp2002@gmail.com>
2026-03-03 16:40:23 -0300

    criado o template das páginas do nav item institucional. Criado a página do secretário.

--- rotas/pagina-inicial-rotas.js
+++ rotas/pagina-inicial-rotas.js
@@ -1,20 +1,32 @@
 import express from "express"
 const router = express.Router();
 
-router.get("/", async(req, res) => {
+router.get("/", async(_req, res) => {
     res.render("paginas/pagina-inicial")
 })
 
-router.get("/NTM/sobre", async(req, res) => {
+router.get("/NTM/sobre", async(_req, res) => {
     res.render("paginas/NTM/sobre-ntm")
 })
 
-router.get("/NTM/equipe", async(_req, res) => {
+router.get("/NTM/equipe", async(__req, res) => {
     res.render("paginas/NTM/nossa-equipe")
 })
 
-router.get("/NTM/na-escola", async(req, res) => {
+router.get("/NTM/na-escola", async(_req, res) => {
     res.render("paginas/NTM/ntm-na-escola")
+})
+
+router.get("/institucional/sobre", async(_req, res) => {
+    res.render("paginas/INSTITUCIONAL/sobre-secretaria")
+})
+
+router.get("/institucional/secretario", async(_req, res) => {
+    res.render("paginas/INSTITUCIONAL/secretario")
+})
+
+router.get("/institucional/organograma", async(_req, res) => {
+    res.render("paginas/INSTITUCIONAL/organograma")
 })
 
 export default router;
